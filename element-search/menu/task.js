950644d0f25d88a903e9863bc72aeebd1efb2e2d
buzh.61 <buzh.61@gmail.com>
2022-04-05 14:11:49 +0300

    Правки кода

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -6,9 +6,12 @@ for (let i in arrLinks) {
     arrLinks[i].onclick = () => {
         let linksWithSub = arrLinks[i].closest('li').querySelector('.menu_sub');
         let nestedLinks = arrLinks[i].closest('li').closest('.menu_sub');
-        console.log(nestedLinks)
+        let linksWithoutSub = arrLinks[i].closest('li').closest('.menu_main');
+        
         if (nestedLinks) {
-            close()
+            close();
+        } else if (linksWithoutSub && !linksWithSub) {
+            close();
         } else if (!linksWithSub.classList.contains('menu_active')) {
             close();
             linksWithSub.classList.add('menu_active');
